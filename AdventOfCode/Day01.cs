03885d20968a5b0e36987cd247b4a430b6720cd1
Tobias Dersén <tobberoth@gmail.com>
2025-12-01 16:53:39 +0100

    Day 1 done

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -3,13 +3,53 @@
 public class Day01 : BaseDay
 {
     private readonly string _input;
+    private int zeroclicks { get; set; }
 
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() {
+        var count = 0;
+        var combination = 50;
+        var lines = _input.Split(Environment.NewLine);
+        foreach (var line in lines) {
+            combination = Spin(combination, line[0], int.Parse(line[1..]));
+            if (combination == 0)
+                count++;
+        }
+        return new(count.ToString());
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() {
+        return new(zeroclicks.ToString());
+    }
+    
+    public int Spin(int combination, char direction, int steps) {
+        var startedAtZero = combination == 0;
+        if (direction == 'R') {
+            combination += steps;
+            if (combination > 99) {
+                zeroclicks += Math.Abs(combination / 100);
+            }
+            combination = combination % 100;
+        } else {
+            combination -= steps;
+            var counted = false;
+            if (combination < 0) {
+                counted = true;
+                zeroclicks += Math.Abs(combination / 100) + 1;
+                if (startedAtZero) zeroclicks -= 1;
+            }
+            combination = combination % 100;
+            if (combination < 0) {
+                combination += 100;
+            } else if (combination == 0) {
+                if (!counted)
+                    zeroclicks++;
+            }
+        }
+        return combination;
+    }
 }
